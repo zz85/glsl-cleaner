bcca343302f7a5baaf7d4297f249083504104a9e
Joshua Koo <zz85nus@gmail.com>
2015-09-27 10:45:19 +0800

    Most basic unused variable linter

--- walk.js
+++ walk.js
@@ -20,7 +20,7 @@ function lint(ast, passes) {
 	var warnings = []; // or pass reporter
 	passes.forEach(function(pass) {
 		var warning = pass(ast);
-		warnings.push(warnings);
+		warnings = warnings.concat(warnings);
 	});
 	return warnings;
 }
@@ -43,30 +43,65 @@ void test(float g, float h) {
 */
 
 function unused_variables(ast, global_declared) {
+	// exception - varying depends on in vs / fs.
 	var declared_list = {};
+	var warnings = [];
 
 	function checkNode(node) {
-		if (node.type === 'decllist') { // stmtlist
-			walk_into(node, 'ident', function(node) {
-				declared_list[node.data] = 0;
-				console.log('ident!', node.data);
-			});
-		}
+
 	}
 
 	simple_walker(ast, function(node) {
-		checkNode(node);
+		if (node.type === 'decllist') {
+			walk_into(node, 'ident', function(node) {
+				declared_list[node.data] = 0;
+				// console.log('ident!', node.data);
+			});
+
+			return;
+		}
+
+		if (node.type === 'expr') {
+			walk_into(node, 'ident', function(node) {
+				if (node.data in declared_list) {
+					declared_list[node.data]++;
+				}
+				else if (global_declared && node.data in global_declared) {
+					global_declared[node.data]++;
+				}
+				else {
+					// warn that variable has not been declared!
+					// console.log('Not found!!', node.data);
+				}
+				// console.log('expr ident!', node.data);
+			});
+
+			return;
+		}
 
 		if (node.type === 'function' && !global_declared) {
 			console.log('function!');
 
-			unused_variables(node, declared_list);
+			warnings = warnings.concat(
+				unused_variables(node, declared_list)
+			);
+
 
 			return true; // break walker
 		}
 	});
 
-	console.log('declared variables', Object.keys(declared_list));
+	console.log('declared variables', declared_list, Object.keys(declared_list));
+
+	for (var variable in declared_list) {
+		if (!declared_list[variable]) {
+			var msg = '[' + variable + '] is not used';
+			console.log(msg);
+			warnings.push(msg)
+		}
+	}
+
+	return warnings;
 	// decl < decllist
 	// stmt / func / functionargs
 }
